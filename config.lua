a3a6d2f089a7d473df5a6c268b9df8b949e4781a
Marat <162355960+MaratWC@users.noreply.github.com>
2025-03-20 21:12:40 +0100

    Improved comments

--- config.lua
+++ config.lua
@@ -8,17 +8,34 @@
 --                                                            |_|                             
 Config = {}
 
-Config.UpdateChecker = true
+---------------- GENERAL SETTINGS ----------------
+Config.UpdateChecker = true -- Enable or disable automatic version checking on resource start
 
+---------------- DEBUG OPTIONS ----------------
 Config.Debug = {
-    ['ERROR'] = true,
-    ['INFO'] = true,
-    ['EXPORTS'] = false,
-    ['RESOURCES'] = false
+    ['ERROR'] = true,    -- Enable or disable the logging of error messages
+    ['INFO'] = true,     -- Enable or disable the logging of information messages
+    ['EXPORTS'] = false, -- Enable or disable the logging of export function calls
+    ['RESOURCES'] = false -- Enable or disable the logging of resource events
 }
 
+---------------- FRAMEWORK ----------------
+-- 'ESX' - https://github.com/esx-framework/esx_core
+-- 'QB' - https://github.com/qbcore-framework/qb-core
+-- 'auto' - Automatically detects the installed framework
 Config.Framework = 'auto' -- ESX / QB / auto
 
+---------------- INVENTORY ----------------
+-- 'ESX' - https://github.com/esx-framework
+-- 'OX' - https://github.com/overextended/ox_inventory
+-- 'QB' - https://github.com/qbcore-framework/qb-inventory
+-- 'auto' - Automatically detects the installed inventory system
 Config.Inventory = 'auto' -- ESX / OX / QB / auto
 
+---------------- NOTIFY ----------------
+-- 'ESX' - https://github.com/esx-framework
+-- 'OX' - https://github.com/overextended/ox_lib
+-- 'QB' - https://github.com/qbcore-framework
+-- 'CHAT' - Uses the default FiveM chat
+-- 'auto' - Automatically detects the installed notification system
 Config.Notify = 'auto' -- ESX / OX / QB / CHAT / auto
