b3f2e2949f60bc0ac55134167e3eecd99d6bca6e
Marat <162355960+MaratWC@users.noreply.github.com>
2025-08-11 00:08:46 +0200

    refactor(update-checker):cleaned code and improved functionality

--- server/update-checker.lua
+++ server/update-checker.lua
@@ -3,37 +3,40 @@ function UpdateChecker(version)
         exports['wix_core']:Debug('ERROR', 'UpdateChecker: version is required')
         return nil
     end
-    
+
     if Config.UpdateChecker == true then
         local resource = GetInvokingResource()
-        local versionCheckUrl = "https://raw.githubusercontent.com/Wix-Development/wix_core/main/update-checker/" .. resource .. ".json"
-        
+        local versionCheckUrl = 'https://raw.githubusercontent.com/Wix-Development/wix_core/main/update-checker/' .. resource .. '.json'
+
         PerformHttpRequest(versionCheckUrl, function(statusCode, responseText, headers)
             if statusCode == 200 then
                 local latestVersionInfo = json.decode(responseText)
                 local latestVersion = latestVersionInfo.version
                 local updateMessage = latestVersionInfo.message
+                local url = latestVersionInfo.url
 
                 if latestVersion ~= version then
-                    print("^3[UPDATE CHECKER]^7 A new version of " .. resource .. " is available!")
-                    print("^3[UPDATE CHECKER]^7 Current version: " .. version)
-                    print("^3[UPDATE CHECKER]^7 Latest version: " .. latestVersion)
-                    print("^3[UPDATE CHECKER]^7 Update message: " .. updateMessage)
-                    if resource == GetCurrentResourceName() then
-                        print("^3[UPDATE CHECKER]^7 Get the latest update here: https://github.com/Wix-Development/wix_core")
-                    else
-                        print("^3[UPDATE CHECKER]^7 Get the latest update here: https://portal.cfx.re/")
+                    print('^3<===========================================================================================>^7')
+                    print('^3[UPDATE CHECKER]^7 A new version of ' .. resource .. ' is available!')
+                    print('^3[UPDATE CHECKER]^7 Current version: ' .. version)
+                    print('^3[UPDATE CHECKER]^7 Latest version: ' .. latestVersion)
+                    if updateMessage ~= 'false' then
+                        print('^3[UPDATE CHECKER]^7 Update message: ' .. updateMessage)
                     end
+                    if url ~= 'false' then
+                        print('^3[UPDATE CHECKER]^7 Download: ' .. url)
+                    end
+                    print('^3<===========================================================================================>^7')
                     return false
                 else
-                    print("^2[UPDATE CHECKER]^7 You are using the latest version of " .. resource .. "!")
+                    print('^2[UPDATE CHECKER]^7 You are using the latest version of ' .. resource .. '!')
                     return true
                 end
             else
-                print("^1[UPDATE CHECKER]^7 Failed to check for updates for resource ".. resource..". HTTP Status Code: " .. statusCode)
+                print('^1[UPDATE CHECKER]^7 Failed to check for updates for resource '.. resource..'. HTTP Status Code: ' .. statusCode)
                 return nil
             end
-        end, "GET", "", {["Content-Type"] = "application/json"})
+        end, 'GET', '', {['Content-Type'] = 'application/json'})
     end
 end
 
